d80d98fe4a57d71451be8e3847ecf6a1c80efbf9
Paulo Souza <paulo.souza@experian.com>
2023-03-29 12:41:11 -0300

    Finished Step 1: Data Ingestion. Finished model training.

--- ingestion.py
+++ ingestion.py
@@ -1,26 +1,52 @@
+'''
+Ingests, process and persists input data present in the folders described in
+the config.json file.
+
+Author: Paulo Souza
+Date: Mar 2023
+'''
+
 import pandas as pd
 import numpy as np
 import os
 import json
 from datetime import datetime
 
-
-
-
-#############Load config.json and get input and output paths
 with open('config.json','r') as f:
-    config = json.load(f) 
+    config = json.load(f)
+current_folders = os.listdir()
 
 input_folder_path = config['input_folder_path']
+if input_folder_path not in current_folders:
+    raise Exception(
+        f"""\
+        {input_folder_path} folder not present in the project directory.
+        Please indicate a valid folder.
+        """
+    )
+
 output_folder_path = config['output_folder_path']
+if output_folder_path not in current_folders:
+    os.mkdir(output_folder_path)
 
+def merge_multiple_dataframes() -> None:
+    '''
+    Checks for datasets, compile them together, and write to an output file
+    '''
+    input_data = [el for el in os.listdir(input_folder_path) if '.csv' in el]
+    with open(os.path.join(output_folder_path, 'ingestedfiles.txt'), 'w') as f:
+        f.write(', '.join(input_data))
 
+    final = pd.DataFrame()
+    for data in input_data:
+        temp = pd.read_csv(os.path.join(input_folder_path, data))
+        final = pd.concat([final, temp], axis=0)
 
-#############Function for data ingestion
-def merge_multiple_dataframe():
-    #check for datasets, compile them together, and write to an output file
-
-
+    final.drop_duplicates(inplace=True)
+    final.to_csv(
+        os.path.join(output_folder_path, 'finaldata.csv'),
+        index=False
+    )
 
 if __name__ == '__main__':
-    merge_multiple_dataframe()
+    merge_multiple_dataframes()
--- training.py
+++ training.py
@@ -1,32 +1,62 @@
-from flask import Flask, session, jsonify, request
+'''
+Trains a Logistic Regression with the processed data available.
+
+Author: Paulo Souza
+Date: Mar 2023
+'''
+
+#rom flask import Flask, session, jsonify, request
 import pandas as pd
-import numpy as np
+#mport numpy as np
 import pickle
 import os
-from sklearn import metrics
-from sklearn.model_selection import train_test_split
+#rom sklearn import metrics
+#rom sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 import json
 
-###################Load config.json and get path variables
 with open('config.json','r') as f:
-    config = json.load(f) 
+    config = json.load(f)
+current_folders = os.listdir()
 
 dataset_csv_path = os.path.join(config['output_folder_path']) 
 model_path = os.path.join(config['output_model_path']) 
+if model_path not in current_folders:
+    os.mkdir(model_path)
 
+def train_model() -> None:
+    '''
+    Trains the model and persists a serialized version of it.
+    '''
 
-#################Function for training the model
-def train_model():
-    
-    #use this logistic regression for training
-    LogisticRegression(C=1.0, class_weight=None, dual=False, fit_intercept=True,
-                    intercept_scaling=1, l1_ratio=None, max_iter=100,
-                    multi_class='warn', n_jobs=None, penalty='l2',
-                    random_state=0, solver='liblinear', tol=0.0001, verbose=0,
-                    warm_start=False)
-    
-    #fit the logistic regression to your data
-    
-    #write the trained model to your workspace in a file called trainedmodel.pkl
+    lr = LogisticRegression(
+        C=0.1,
+        class_weight=None,
+        dual=False,
+        fit_intercept=True,
+        intercept_scaling=1,
+        #l1_ratio=0.75,
+        max_iter=200,
+        multi_class='auto',
+        n_jobs=1,
+        penalty='l2',
+        random_state=42,
+        solver='liblinear',
+        tol=0.0001,
+        verbose=0,
+        warm_start=False
+    )
 
+    train_data = pd.read_csv(os.path.join(dataset_csv_path, 'finaldata.csv'))
+    train_data.drop('corporation', axis=1, inplace=True)
+
+    x_train = train_data.drop('exited', axis=1)
+    y_train = train_data['exited']
+    
+    lr.fit(x_train, y_train)
+    
+    with open(os.path.join(model_path, 'trainedmodel.pkl'), 'wb') as f:
+        pickle.dump(lr, f)
+
+if __name__ == '__main__':
+    train_model()
